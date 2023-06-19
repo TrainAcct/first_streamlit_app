55cc80e240b230766838127923f1036e9d722cd4
TrainAcct <137099598+TrainAcct@users.noreply.github.com>
2023-06-19 12:34:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Egg")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
